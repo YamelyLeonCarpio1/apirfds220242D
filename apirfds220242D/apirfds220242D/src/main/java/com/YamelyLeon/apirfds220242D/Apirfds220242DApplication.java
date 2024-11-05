ef7e593fc24f86b71ca448e9b5dc2364fb7c8d32
YamelyLeonCarpio1 <yamelyleon4@gmail.com>
2024-11-04 22:58:16 -0500

    primer commit

---
+++ apirfds220242D/apirfds220242D/src/main/java/com/YamelyLeon/apirfds220242D/Apirfds220242DApplication.java
@@ -0,0 +1,13 @@
+package com.YamelyLeon.apirfds220242D;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apirfds220242DApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apirfds220242DApplication.class, args);
+	}
+
+}
---
+++ apirfds220242D/apirfds220242D/src/test/java/com/YamelyLeon/apirfds220242D/Apirfds220242DApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YamelyLeon.apirfds220242D;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apirfds220242DApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
